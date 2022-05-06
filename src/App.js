e33b54a0d269f1f62918fe85abc1eacac4ab177b
shova29 <halima2996@gmail.com>
2022-05-07 03:11:07 +0600

    banner section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Pages/Shared/Header/Header";
-
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <div>
       <Header></Header>
+      <Home></Home>
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,63 +1,75 @@
-import React from "react";
+import { Transition } from "@headlessui/react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <nav className="bg-rose-600">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <img className="h-8 w-8" src={""} alt="logo" />
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <a
-                    href="#"
-                    className=" hover:text-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Perfume Pungent
-                  </a>
+    <nav className="bg-rose-600 shadow-md w-full h-20">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <img className="h-8 w-8" src="" alt="logo" />
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a
+                  href="#"
+                  className=" hover:bg-text-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Perfume Pungent
+                </a>
 
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:text-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    About
-                  </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-text-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  About
+                </a>
 
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:text-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Manage Items
-                  </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-text-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Manage Items
+                </a>
 
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:text-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Add Items
-                  </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-text-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Add Items
+                </a>
 
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:text-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-text-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  My Items
+                </a>
+                <div class="flex items-center lg:ml-auto  mb-2 lg:mb-0">
+                  <button
+                    type="button"
+                    className="ml-52 inline-block px-6 py-2.5 mr-2 bg-transparent text-white font-medium text-xs leading-tight uppercase rounded hover:text-rose-500 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+                    data-mdb-ripple="true"
+                    data-mdb-ripple-color="light"
                   >
-                    My Items
-                  </a>
+                    Login
+                  </button>
                 </div>
               </div>
             </div>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                type="button"
-                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
                 <svg
                   className="block h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
@@ -67,15 +79,15 @@ const Header = () => {
                   aria-hidden="true"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 6h16M4 12h16M4 18h16"
                   />
                 </svg>
-
+              ) : (
                 <svg
-                  className="hidden h-6 w-6"
+                  className="block h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -83,57 +95,101 @@ const Header = () => {
                   aria-hidden="true"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <Transition
+        show={isOpen}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        {(ref) => (
+          <div className="md:hidden" id="mobile-menu">
+            <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <a
+                href="#"
+                className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Blogs
+              </a>
+
+              <a
+                href="#"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                About
+              </a>
+
+              <a
+                href="#"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Manage Items
+              </a>
+
+              <a
+                href="#"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Add Items
+              </a>
+
+              <a
+                href="#"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                My Items
+              </a>
+              <button
+                type="button"
+                className=" mb-2 lg:mb-0 inline-block px-6 py-2.5 mr-2 bg-transparent text-white font-medium text-xs leading-tight uppercase rounded hover:text-rose-500 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+              >
+                Login
               </button>
             </div>
           </div>
-        </div>
-
-        <div className="md:hidden" id="mobile-menu">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a
-              href="#"
-              className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              About
-            </a>
-
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Manage Items
-            </a>
-
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Add Items
-            </a>
-
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              My Items
-            </a>
-
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Login
-            </a>
-          </div>
-        </div>
-      </nav>
-    </div>
+        )}
+      </Transition>
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1366 420"
+        preserveAspectRatio="none"
+        className="svg absolute hidden lg:block"
+        style={{
+          height: "740px",
+          width: "100%",
+          zIndex: -10,
+          overflow: "hidden",
+        }}
+      >
+        <defs>
+          <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+            <stop stop-color="hsla(356, 100%, 95%, 1)" offset="0%"></stop>
+            <stop stop-color="hsla(356, 100%, 95%, 1)" offset="100%"></stop>
+          </linearGradient>
+        </defs>
+        <path
+          fill="url(#sw-gradient-0)"
+          d="M 0.351 264.418 C 0.351 264.418 33.396 268.165 47.112 270.128 C 265.033 301.319 477.487 325.608 614.827 237.124 C 713.575 173.504 692.613 144.116 805.776 87.876 C 942.649 19.853 1317.845 20.149 1440.003 23.965 C 1466.069 24.779 1440.135 24.024 1440.135 24.024 L 1440 0 L 1360 0 C 1280 0 1120 0 960 0 C 800 0 640 0 480 0 C 320 0 160 0 80 0 L 0 0 L 0.351 264.418 Z"
+        ></path>
+      </svg>
+    </nav>
   );
 };
 export default Header;
